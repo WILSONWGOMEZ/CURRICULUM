98af73ac23db87d1d4e68da03da661405e633e77
WILSONWGOMEZ <wilsonm123_@hotmail.com>
2023-11-22 00:04:31 -0500

    INICIOS

---
+++ Scrips.js
@@ -0,0 +1,4 @@
+// JavaScript para cambiar el color de fondo al hacer clic en la sección de perfil
+function seleccionar() {
+    document.getElementById("INICIO").style.backgroundColor = "#f0f0f0"; // Cambia el color de fondo según tu preferencia
+}
